3a3303ce51422f8a28fab006022e8eea2277b2c5
Joao Pedro Marcondes <joaopopm@usp.br>
2024-10-18 17:24:04 -0300

    Alterei alguns arquivos

--- psort14582570.c
+++ psort14582570.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("oi\n");
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        fprintf(stderr, "Erro na passagem de parâmetros. Execute no formato: ./psort<nusp> <entrada> <saída> <threads>");
+        return -1;
+    }
+    FILE* entrada = fopen(argv[1], "r");
+    FILE* saida = fopen(argv[2], "w");
+    int* num;
+    while (fscanf(entrada, "%d", num) > 0) {
+        printf("Olá, %d!\n", *num);
+    }
+    printf("\n");
     return 0;
 }
