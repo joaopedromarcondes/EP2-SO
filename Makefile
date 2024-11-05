703d47fa9dfa0d61f1fa6b6c7661ae939bccaf33
Joao Pedro Marcondes <joaopopm@usp.br>
2024-11-05 20:14:25 -0300

    Finalmente entendi como ler de um arquivo do jeito que o professor quer

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run:
-	gcc psort14582570.c -Wall -Werror -pthread -std=c11 -O
-	time -v ./a.out entrada.txt saida.txt 3
+	gcc psort14582570.c 
+	./a.out entrada.txt saida.txt 3
 	rm a.out
 	
--- psort14582570.c
+++ psort14582570.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void ordena(int* chaves, FILE* arq) {
+
+}
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
+    /* if (argc != 4) {
         fprintf(stderr, "Erro na passagem de parâmetros. Execute no formato: ./psort<nusp> <entrada> <saída> <threads>");
         return -1;
     }
@@ -12,6 +25,23 @@ int main(int argc, char *argv[]) {
     while (fscanf(entrada, "%d", num) > 0) {
         printf("Olá, %d!\n", *num);
     }
-    printf("\n");
+    printf("\n"); */
+
+
+    FILE * entrada = fopen("testes/10_registros_ja_ordenados.dat", "r");
+    int a;
+    fread(&a, 4, 1, entrada);
+    printf("%d\n", a);
+    unsigned char b[96];
+    fread(&b, 1, 96, entrada);
+    //printf("%d\n", b);
+    int c;
+    fread(&c, 4, 1, entrada);
+    printf("%d\n", c);
+
+
+    if (fclose(entrada))
+        perror("fclose error");
+
     return 0;
 }
